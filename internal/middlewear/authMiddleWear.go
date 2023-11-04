46436678cdd239c0c55842fe3d932c4f9abfbed3
soma <somashekharm4159@gmail.com>
2023-11-04 15:22:59 +0530

    updated middlewear 1

--- internal/middlewear/authMiddleWear.go
+++ internal/middlewear/authMiddleWear.go
@@ -12,36 +12,41 @@ import (
 
 const TokenIdKey key = "2"
 
+// Auth is a middleware function that handles authentication.
 func (m Middlewear) Auth(next gin.HandlerFunc) gin.HandlerFunc {
 	return func(c *gin.Context) {
+
 		ctx := c.Request.Context()
-		traceId, ok := ctx.Value(TraceIdKey).(string)
+		traceID, ok := ctx.Value(TraceIdKey).(string)
 		if !ok {
-			log.Error().Msg("tokenid not string")
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"msg": http.StatusInternalServerError})
-			return
+			if !ok {
+				log.Error().Msg("TraceID not a string")
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+				return
+			}
 		}
-		log.Info().Str("traceid", "traceId").Msg("in authentication")
+		log.Info().Str("traceID", traceID).Msg("In authentication")
 
-		authHead := c.Request.Header.Get("Authorization")
+		authHeader := c.Request.Header.Get("Authorization")
 
-		parts := strings.Split(authHead, " ")
+		parts := strings.Split(authHeader, " ")
 		// Checking the format of the Authorization header
 		if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
 			// If the header format doesn't match required format, log and send an error
 			err := errors.New("expected authorization header format: Bearer <token>")
-			log.Error().Err(err).Str("Trace Id", traceId).Send()
+			log.Error().Err(err).Str("Trace ID", traceID).Send()
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 			return
 		}
 
-		regClaims, err := m.a.ValidateToken(parts[1])
+		claims, err := m.a.ValidateToken(parts[1])
 		if err != nil {
-			log.Info().Msg("Auth failed")
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": http.StatusUnauthorized})
+			log.Info().Msg("Authentication failed")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
 		}
 
-		ctx = context.WithValue(ctx, TokenIdKey, regClaims)
+		ctx = context.WithValue(ctx, TokenIdKey, claims)
 		req := c.Request.WithContext(ctx)
 		c.Request = req
 
