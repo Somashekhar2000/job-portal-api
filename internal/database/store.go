5c90d5f506a2afe76ee0d7cf919b7573a10f0811
soma <somashekharm4159@gmail.com>
2023-11-04 14:11:14 +0530

    updated store.go

--- internal/database/store.go
+++ internal/database/store.go
@@ -5,27 +5,22 @@ import (
 	"project/internal/model"
 )
 
+// CreateTable initializes and updates database tables.
 func CreateTable() {
-	// Drop the table student if it exists
+	// Open the database connection
 	db, err := Open()
 	if err != nil {
+		log.Fatalln("Failed to open the database:", err)
+	}
+
+	// Drop the "User" table if it exists
+	if err = db.Migrator().DropTable(&model.User{}); err != nil {
+		log.Fatalln("Failed to drop the 'User' table:", err)
+	}
+
+	// Create or update the "User" table
+	if err = db.Migrator().AutoMigrate(&model.User{}); err != nil {
 		log.Fatalln(err)
 	}
-	err = db.Migrator().DropTable(&model.User{})
-	if err != nil {
-		log.Fatalln(err)
-	}
-	err = db.Migrator().AutoMigrate(&model.User{})
-	if err != nil {
-		log.Fatalln(err)
-	}
-	// err = db.Migrator().AutoMigrate(&model.Company{})
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// err = db.Migrator().AutoMigrate(&model.Job{})
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
 
 }
