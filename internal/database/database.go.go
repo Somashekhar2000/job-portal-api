854d2d54bb514df13cc4d63e8c89fb285b4a556c
soma <somashekharm4159@gmail.com>
2023-11-04 14:07:14 +0530

    updated database.go

--- internal/database/database.go.go
+++ internal/database/database.go.go
@@ -13,43 +13,53 @@ import (
 
 // Open initializes a database connection and returns a GORM database instance.
 func Open() (*gorm.DB, error) {
+
 	dsn := "host=localhost user=postgres password=1234 dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
 	}
 	return db, nil
 }
 
+// Connection initializes the database connection and performs migrations for relevant models.
 func Connection() (*gorm.DB, error) {
-	log.Info().Msg("main : Started : Initializing db support")
+
+	log.Info().Msg("database: Initializing database support")
+
 	db, err := Open()
 	if err != nil {
-		return nil, fmt.Errorf("connecting to db %w", err)
+		return nil, fmt.Errorf("database: connection error: %w", err)
 	}
+
 	pg, err := db.DB()
 	if err != nil {
-		return nil, fmt.Errorf("failed to get database instance %w ", err)
+		return nil, fmt.Errorf("database: failed to get database instance: %w", err)
 	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 
+	// Check the database connection status
 	err = pg.PingContext(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("database is not connected: %w ", err)
+		return nil, fmt.Errorf("database: connection error: %w", err)
 	}
-	//db.Migrator().DropTable(&model.User{}, &model.Company{}, &model.Job{})
+
+	// Perform model migrations
 	err = db.Migrator().AutoMigrate(&model.User{})
 	if err != nil {
-		return nil, fmt.Errorf("auto migration failed: %w ", err)
+		return nil, fmt.Errorf("database: auto migration failed for User model: %w", err)
 	}
+
 	err = db.Migrator().AutoMigrate(&model.Company{})
 	if err != nil {
-		return nil, fmt.Errorf("auto migration failed: %w ", err)
+		return nil, fmt.Errorf("database: auto migration failed for Company model: %w", err)
 	}
+
 	err = db.Migrator().AutoMigrate(&model.Job{})
 	if err != nil {
-		return nil, fmt.Errorf("auto migration failed: %w ", err)
+		return nil, fmt.Errorf("database: auto migration failed for Job model: %w", err)
 	}
 	return db, nil
 }
